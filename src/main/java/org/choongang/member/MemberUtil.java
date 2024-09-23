0d07922ad54fc61e5a840e58e0559965a0193142
siwon9 <siwon10823@gmail.com>
2024-09-24 07:28:47 +0900

    authority

--- src/main/java/org/choongang/member/MemberUtil.java
+++ src/main/java/org/choongang/member/MemberUtil.java
@@ -2,13 +2,10 @@ package org.choongang.member;
 
 import lombok.RequiredArgsConstructor;
 import org.choongang.member.constants.Authority;
-import org.choongang.member.entities.Authorities;
 import org.choongang.member.entities.Member;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
