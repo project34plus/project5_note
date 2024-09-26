602e4bd6e9f31089e6341d045422d77cffb49209
hyeon12 <seojh912_@naver.com>
2024-09-26 20:07:07 +0900

    노트(게시글) 작성 서비스 수정

--- src/main/java/org/choongang/notedata/services/NoteSaveService.java
+++ src/main/java/org/choongang/notedata/services/NoteSaveService.java
@@ -9,6 +9,7 @@ import org.choongang.notedata.controllers.RequestNoteData;
 import org.choongang.notedata.entities.NoteData;
 import org.choongang.notedata.exceptions.NoteDataNotFoundException;
 import org.choongang.notedata.repositories.NoteDataRepository;
+import org.choongang.notedata.services.config.NoteConfigInfoService;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -19,24 +20,29 @@ import java.util.Objects;
 public class NoteSaveService {
     private final NoteDataRepository noteDataRepository;
     private final FileUploadDoneService uploadDoneService;
+    private final NoteConfigInfoService configInfo;
     private final MemberUtil memberUtil;
 
     public void save(RequestNoteData form) {
 
         String mode = Objects.requireNonNullElse(form.getMode(), "register");
-        Long nid = form.getNoteSeq();
+        Long noteSeq = form.getNoteSeq();
+        //Long nid = form.getNoteSeq();
 
         NoteData noteData = null;
-        if (mode.equals("update") && nid != null) { // 수정
-            noteData = noteDataRepository.findById(nid).orElseThrow(NoteDataNotFoundException::new);
+        if (mode.equals("update") && noteSeq != null) { // 수정
+            noteData = noteDataRepository.findById(noteSeq).orElseThrow(NoteDataNotFoundException::new);
         } else { // 추가
-            noteData = new NoteData();
-            noteData.setGid(form.getGid());
+            noteData = NoteData.builder()
+                    .note(configInfo.get(form.getNid()))
+                    .gid(form.getGid())
+                    .build();
             if (memberUtil.isLogin()) {
                 Member member = memberUtil.getMember();
                 noteData.setEmail(member.getEmail());
                 noteData.setUsername(member.getUserName());
             }
+
         }
 
         /* 추가, 수정 공통 처리 S */
